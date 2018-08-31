30e9d13187faa748cc374c35a959574787e72c58
EthanFe <sneakcow@gmail.com>
2018-08-31 10:42:06 -0500

    Redo results display to list all results and allow re-sorting

--- cli.rb
+++ cli.rb
@@ -2,16 +2,27 @@ require_relative "./data_requester.rb"
 require_relative "./place.rb"
 
 def run_interface
+  mode = :search
+  sort_type = "rating"
   while true
-    keyword = ask_for_keyword
-    if keyword
-      if keyword != "quit"
-        api_data = Data_Requester.get_data_for_keyword(keyword)
-        places = Place.parse_api_data(api_data["results"])
-        top_places = calculate_top_places(places)
-        print_top_places(top_places)
-      else
-        return
+    keyword = ask_for_keyword(mode, sort_type)
+    if keyword == "quit"
+      return
+    end
+
+    case mode
+    when :search
+      if keyword
+        places = new_search(keyword)
+        print_places(places, sort_type)
+        mode = :sort_type
+      end
+    when :sort_type
+      if keyword == "distance" || keyword == "rating"
+        sort_type = keyword
+        print_places(places, sort_type)
+      elsif keyword == "new"
+        mode = :search
       end
     end
     # input = ask_for_input
@@ -24,22 +35,37 @@ def run_interface
   end
 end
 
-def calculate_top_places(places)
-  closest_places = places.sort_by {|place| place.distance}
-  best_places = (places.sort_by {|place| place.rating}).reverse
-
-  top_places = closest_places[0..2] # should prob not hardcode this
-  top_places.concat(best_places[0..2]).uniq
+def new_search(keyword)
+  api_data = Data_Requester.get_data_for_keyword(keyword)
+  Place.parse_api_data(api_data["results"])
 end
 
-def print_top_places(places)
+def print_places(places, sort_by)
+  places = sort_places(places, sort_by)
   places.each do |place|
-    puts "#{place.name}: distance #{place.distance} miles, rating #{place.rating}/5 (#{place.address})"
+    puts "#{place.name}: distance #{place.distance} miles, rating #{place.rating} / 5 (#{place.address})"
   end
 end
 
-def ask_for_keyword
-  puts "Enter restaurant type to search for, or quit to exit"
+def sort_places(places, sort_by)
+  case sort_by
+  when "rating"
+    puts "Displaying sorted by rating:"
+    places = (places.sort_by {|place| place.rating}).reverse
+  when "distance"
+    puts "Displaying sorted by distance:"
+    places.sort_by! {|place| place.distance}
+  end
+  places
+end
+
+def ask_for_keyword(mode, sort_type)
+  case mode
+  when :search
+    puts "Enter restaurant type to search for, or quit to exit"
+  when :sort_type
+    puts "Enter 'rating' or 'distance' to re-sort, 'new' to start a new search, or 'quit' to exit"
+  end
   gets.chomp
 end
 
